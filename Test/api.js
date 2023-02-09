74f6a3079b46eb63b7dd29d15d82411a4898b00e
danieldoh <doh7337@gmail.com>
2023-02-08 21:45:43 -0500

    hello doh

--- Test/api.js
+++ Test/api.js
@@ -1,95 +1,57 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var axios = require('axios');
-var fs = require('fs');
-function getScoreCard(url) {
-    return __awaiter(this, void 0, void 0, function () {
-        var api_url, repo_url, repo_url_len, sliced_repo_url, api_repo, options, jsonfile_name;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    api_url = new String("https://api.securityscorecards.dev/projects/");
-                    repo_url = new String(url);
-                    repo_url_len = repo_url.length;
-                    sliced_repo_url = repo_url.slice(8, repo_url_len);
-                    api_repo = api_url.concat(sliced_repo_url.toString());
-                    options = {
-                        method: 'GET',
-                        url: api_repo,
-                        params: { category: 'all', count: 2 }
-                    };
-                    jsonfile_name = './Scorecard.json';
-                    return [4 /*yield*/, axios
-                            .request(options)
-                            .then(function (response) {
-                            var jsonString = JSON.stringify(response.data, null, 2);
-                            fs.writeFileSync(jsonfile_name, jsonString, {
-                                flag: 'w'
-                            });
-                        })["catch"](function (error) {
-                            console.error(error);
-                        })];
-                case 1:
-                    _a.sent();
-                    return [2 /*return*/, jsonfile_name];
-            }
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios = require('axios');
+const fs = require('fs');
+async function getScoreCard(url) {
+    var api_url = new String("https://api.securityscorecards.dev/projects/");
+    var repo_url = new String(url);
+    var repo_url_len = repo_url.length;
+    var sliced_repo_url = repo_url.slice(8, repo_url_len);
+    var api_repo = api_url.concat(sliced_repo_url.toString());
+    const options = {
+        method: 'GET',
+        url: api_repo,
+        params: { category: 'all', count: 2 },
+    };
+    var jsonfile_name = './Scorecard.json';
+    await axios
+        .request(options)
+        .then(function (response) {
+        const jsonString = JSON.stringify(response.data, null, 2);
+        fs.writeFileSync(jsonfile_name, jsonString, {
+            flag: 'w'
         });
+    })
+        .catch(function (error) {
+        console.error(error);
     });
+    return jsonfile_name;
 }
-//console.log(getScoreCard("https://github.com/cloudinary/cloudinary_npm"));
-//console.log(getScoreCard("https://github.com/lodash/lodash"));
-//console.log(getScoreCard("https://github.com/nullivex/nodist"));
+const { Octokit } = require("@octokit/core");
+async function myfunction(url) {
+    const octokit = await new Octokit({
+        auth: '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'
+    });
+    const response = await octokit.request('GET /licenses', {});
+    console.log(response);
+    console.log(url);
+}
+myfunction.length;
 function getBusFactor() {
-    var bf_subscore = 5;
+    const bf_subscore = 5;
     return bf_subscore;
 }
 function getLicense() {
-    var li_subscore = 3;
+    const li_subscore = 3;
     return li_subscore;
 }
 function getRampUp() {
-    var ramp_subscore = 6;
+    const ramp_subscore = 6;
     return ramp_subscore;
 }
 function getResponsive() {
-    var resp_subscore = 4;
+    const resp_subscore = 4;
     return resp_subscore;
 }
-module.exports = { getScoreCard: getScoreCard, getBusFactor: getBusFactor, getLicense: getLicense, getRampUp: getRampUp, getResponsive: getResponsive };
-// need to implement how to get into the github repo when the url is 'npm' (need some think)
-// 'npm' URLs cause error with axios.
-// need to implement how to read json file and use them to calculate each sub-score (not too hard)
+module.exports = { getScoreCard, getBusFactor, getLicense, getRampUp, getResponsive, myfunction };
+//# sourceMappingURL=api.js.map
--- Test/api.ts
+++ Test/api.ts
@@ -1,8 +1,7 @@
 const axios = require('axios')
 const fs = require('fs')
-//import { Octokit } from "octokit";
 
-async function getScoreCard(url) {
+async function getScoreCard(url: string) {
     var api_url = new String("https://api.securityscorecards.dev/projects/");
     var repo_url = new String(url);
     var repo_url_len = repo_url.length
@@ -17,13 +16,13 @@ async function getScoreCard(url) {
     var jsonfile_name = './Scorecard.json'
     await axios
         .request(options)
-        .then(function (response) {
+        .then(function (response: any) {
             const jsonString = JSON.stringify(response.data, null, 2)
             fs.writeFileSync(jsonfile_name, jsonString, {
                 flag: 'w'
             })
         })
-        .catch(function (error) {
+        .catch(function (error: any) {
             console.error(error);
         });
     return jsonfile_name
@@ -37,6 +36,73 @@ async function getScoreCard(url) {
 //console.log(getScoreCard("https://github.com/cloudinary/cloudinary_npm"));
 //console.log(getScoreCard("https://github.com/lodash/lodash"));
 //console.log(getScoreCard("https://github.com/nullivex/nodist"));
+//const { graphql } = require("@octokit/graphql");
+//import type { GraphQlQueryResponseData } from "@octokit/graphql";
+//const graphqlWithAuth = graphql.defaults({
+//    headers: {
+//        authorization: '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',
+//    },
+//});
+//
+////export {}
+//
+//const { lastIssues } = graphql({
+//  query: `query lastIssues($owner: String!, $repo: String!, $num: Int = 3) {
+//    repository(owner:$owner, name:$repo) {
+//      issues(last:$num) {
+//        edges {
+//          node {
+//            title
+//          }
+//        }
+//      }
+//    }
+//  }`,
+//  owner: "octokit",
+//  repo: "graphql.js",
+//  headers: {
+//    authorization: `token secret123`,
+//  },
+//});
+
+//console.log(lastIssues);
+
+//const { repository } = async () => Promise<void> {
+//    await graphqlWithAuth (
+//        `{
+//            repository(owner: "octokit", name: "graphql.js") {
+//                issues(last: 3) {
+//                    edges {
+//                        node {
+//                            title
+//                        }
+//                    }
+//                }
+//            }
+//        }`
+//    ),
+//};
+const { Octokit } = require("@octokit/core");
+
+export {}
+
+async function myfunction(url: string)  {
+    const octokit = await new Octokit({
+        auth: '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'
+    });
+    //const response = await octokit.request('GET /repos/{owner}/{repo}/license', {
+    //    owner: 'danieldoh',
+    //    repo: '2KIM_DOH_Project'
+
+    //})
+    const response = await octokit.request('GET /licenses', {})
+    console.log(response);
+    console.log(url);
+}
+
+myfunction.length;
+
+
 
 function getBusFactor() {
     const bf_subscore = 5
@@ -58,4 +124,4 @@ function getResponsive() {
     return resp_subscore
 }
 
-module.exports = { getScoreCard, getBusFactor, getLicense,  getRampUp, getResponsive };
+module.exports = { getScoreCard, getBusFactor, getLicense,  getRampUp, getResponsive, myfunction };
