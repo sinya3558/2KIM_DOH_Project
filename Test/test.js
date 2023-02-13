f007a1ce3a306383e7779f98bba79a9556aea70a
So-Won-Kim <kim3007@purdue.edu>
2023-02-13 05:48:16 -0500

    final

--- Test/test.js
+++
@@ -1,9 +0,0 @@
-import { GithubProvider } from 'github-repository-provider';
-
-const config = GithubProvider.optionsFromEnvironment(process.env);
-const provider = new GithubProvider(config);
-const repository = await provider.repository(`myuser/repo1`);
-
-for async (const entry of repository.entries('\*_/_.md')) {
-  console.log(entry.name);
-}
